423d26030811ef48d418a7add083828bd2886a1b
ymgs3 <y.taka1234567890@gmail.com>
2022-04-26 00:12:07 +0900

    体と腕を作るところまで

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # houdini_rpc_root
+
+## 実行方法
+基本はVisual Studio Code で記述して F5 で実行する
+そのためにまずHoudini側の Python Source Editor で以下を実行しておく
+```
+import hrpyc
+hrpyc.start_server()
+```
---
+++ sandbox.py
@@ -0,0 +1,17 @@
+import os
+# Houdini 上のpythonならhouはデフォルトインポートされているみたいなのでインポート処理を飛ばす
+# 環境変数取れたら Houdini　上と判断
+if not 'HFS' in os.environ:
+    try:
+        import hrpyc
+        connection, hou = hrpyc.import_remote_module()
+    except:
+        # 最後に定義されているhouのautocompleteが効くみたいなので例外側でインポート　
+        import hou
+
+# https://www.youtube.com/playlist?list=PLXNFA1EysfYmpOEGcK1x_r6g_h2dOwAC4
+
+vol:hou.SopNode = hou.node("/obj/geo1/paintsdfvolume1")
+parm:hou.Parm =  vol.parm("stroke6_data")
+parm.asCode()
+print(parm.asCode())
